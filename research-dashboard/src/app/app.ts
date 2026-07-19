e91197b9aaf87970546562c517a2a3d927cd4e55
udan-insith <udaninsith23613@gmail.com>
2026-07-19 09:50:53 +0530

    Update app.ts

--- research-dashboard/src/app/app.ts
+++ research-dashboard/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './research-dashboard/research-dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('research-dashboard');
-}
+export class App {}
