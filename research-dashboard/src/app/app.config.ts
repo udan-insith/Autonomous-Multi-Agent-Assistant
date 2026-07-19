9a65fd44415d570b4d6e663c9227985f1e13d7c1
udan-insith <udaninsith23613@gmail.com>
2026-07-19 09:49:46 +0530

    Update app.config.ts

--- research-dashboard/src/app/app.config.ts
+++ research-dashboard/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
